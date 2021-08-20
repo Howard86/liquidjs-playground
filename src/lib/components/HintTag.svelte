d24311193ea65c1d13e733e889cb24d83e49ce02
Howard Tai <howard@howardism.dev>
2021-08-20 09:55:51 +0800

    feat(ui): add HintTag with Hoverable

---
+++ src/lib/components/HintTag.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Hoverable from './Hoverable.svelte';
+
+	export let displayText = '';
+	export let hoverText = '';
+</script>
+
+<Hoverable let:hovering={active}>
+	<span
+		class="px-2 py-1 flex-none items-center text-xs rounded-md font-semibold text-green-500 bg-green-50 hover:bg-green-500 hover:text-green-50"
+	>
+		{#if active}
+			{hoverText}
+		{:else}
+			{displayText}
+		{/if}
+	</span>
+</Hoverable>
---
+++ src/lib/components/Hoverable.svelte
@@ -0,0 +1,15 @@
+<script>
+	let hovering;
+
+	function enter() {
+		hovering = true;
+	}
+
+	function leave() {
+		hovering = false;
+	}
+</script>
+
+<li on:mouseenter={enter} on:mouseleave={leave}>
+	<slot {hovering} />
+</li>
